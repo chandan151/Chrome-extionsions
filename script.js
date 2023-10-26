5962994639ea8d54c6bba397708984fa47d10bc6
Chandan Sharma <63304840+chandan151@users.noreply.github.com>
2023-10-26 13:25:09 -0600

    Added all the files that were on lab desktop

---
+++ script.js
@@ -0,0 +1,21 @@
+const logo = document.getElementById("logoUleth");
+
+const updateLogo = document.createElement("h1");
+updateLogo.textContent = "University of Bestbridge";
+updateLogo.style.color = "white";
+
+logo.replaceWith(updateLogo);
+
+const brandHeader = document.getElementsByClassName("brand-header");
+console.log(brandHeader);
+
+brandHeader[0].textContent = "New Media 4 Life";
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(request.message);
+        document.body.style.backgroundColor = request.message;
+        sendResponse({ message: "Received message!" });
+    }
+);
+
